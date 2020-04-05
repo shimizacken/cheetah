ec880024fa5846165d8087ba0a3a8e083dcf1f3b
shimizacken <shimizacken@gmail.com>
2020-04-05 13:14:38 +0200

    tests added

--- client/src/features/chat/state/chatMessagesReducer.js
+++ client/src/features/chat/state/chatMessagesReducer.js
@@ -1,13 +1,35 @@
-import { PUBLISH_MESSAGE } from "./tests/chatMessagesConstants";
+import { PUBLISH_MESSAGE, EDIT_MESSAGE, DELETE_MESSAGE } from "./tests/chatMessagesConstants";
+import produce from "immer";
 
-const messagesInitialState = [];
+const messagesInitialState = {};
 
 export const messages = (state = messagesInitialState, action) => {
   if (action.type === PUBLISH_MESSAGE) {
-    return [
+    return {
       ...state,
-      action.message
-    ]
+      [action.message.id]: action.message
+    };
+  }
+
+  if (action.type === EDIT_MESSAGE) {
+    const nextState = produce(state, draftState => {
+      draftState[action.message.id] = action.message;
+
+      return draftState;
+    });
+
+    return nextState;
+  }
+
+  if (action.type === DELETE_MESSAGE) {
+    const nextState = produce(state, draftState => {
+      console.log(action.messageId);
+      draftState[action.messageId].deleted = true;
+
+      return draftState;
+    });
+
+    return nextState;
   }
 
   return state;
--- client/src/features/chat/state/tests/chatMessagesReducer.test.js
+++ client/src/features/chat/state/tests/chatMessagesReducer.test.js
@@ -1,6 +1,6 @@
 import { randomAction } from '../../../../state/randomAction';
 import { messages } from '../chatMessagesReducer';
-import { publishMessage } from './chatMessagesActions';
+import { publishMessage, editMessage, deleteMessage } from './chatMessagesActions';
 
 describe('chatMessagesReducer test suite', () => {
   describe('defaults', () => {
@@ -12,21 +12,101 @@ describe('chatMessagesReducer test suite', () => {
   });
 
   describe('publish message test suite', () => {
-    it('should add new message', () => {
+    it('should add single message', () => {
       const message = {
         id: '1a2s3d4f5g',
-        text: 'hello all!'
+        text: 'hello all!',
+        date: 1586083281242,
+        edited: false,
+        deleted: false
       };
 
       expect(messages(undefined, publishMessage(message))).toMatchSnapshot();
     });
+
+    it('should add multiple messages', () => {
+      const state = {
+        '1a2s3d4f5g': {
+          id: '1a2s3d4f5g',
+          text: 'hello all!',
+          date: 123,
+          edited: false,
+          deleted: false
+        },
+        '4q5w6e64r': {
+          id: '4q5w6e64r',
+          text: 'Hi!',
+          date: 456,
+          edited: false,
+          deleted: false
+        }
+      };
+
+      const message = {
+        id: '1z32c15c64d',
+        text: 'Hi there!',
+        date: 789,
+        edited: false,
+        deleted: false
+      };
+  
+      expect(messages(state, publishMessage(message))).toMatchSnapshot();
+    });
   });
 
   describe('edit message test suite', () => {
-    it.todo('should add new message');
+    it('should edit message', () => {
+      const messageId = '4q5w6e64r';
+      const state = {
+        '1a2s3d4f5g': {
+          id: '1a2s3d4f5g',
+          text: 'hello all!',
+          date: 123,
+          edited: false,
+          deleted: false
+        },
+        messageId: {
+          id: '4q5w6e64r',
+          text: 'Hi!',
+          date: 456,
+          edited: false,
+          deleted: false
+        }
+      };
+    
+      const message = {
+        id: messageId,
+        text: 'Hi to all, I edited this message!',
+        date: 987,
+        edited: true,
+        deleted: false
+      };
+      
+      expect(messages(state, editMessage(message))).toMatchSnapshot();
+    });
   });
 
-  describe('delete message test suite', () => {
-    it.todo('should add new message');
+  describe.only('delete message test suite', () => {
+    it('should delete message by id', () => {
+        const messageId = '1a2b3c4d';
+        const state = {
+          '1a2s3d4f5g': {
+            id: '1a2s3d4f5g',
+            text: 'hello all!',
+            date: 123,
+            edited: false,
+            deleted: false
+          },
+          [messageId]: {
+            id: messageId,
+            text: 'Hi!',
+            date: 456,
+            edited: false,
+            deleted: false
+          }
+        };
+      
+        expect(messages(state, deleteMessage(messageId))).toMatchSnapshot();
+    });
   });
 });
