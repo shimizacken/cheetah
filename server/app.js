030717bd0893aa6e8795aebf77272c19e36fc6d6
shimizacken <shimizacken@gmail.com>
2020-04-05 14:53:23 +0200

    websocket added

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatWS = require('./routes/chat');
 
 var app = express();
 
---
+++ server/routes/chat.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 9500 })
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+});
+
+module.exports = wss;
