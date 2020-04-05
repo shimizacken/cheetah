94a490efd874218c6bd60758b689e1dcc6a75904
shimizacken <shimizacken@gmail.com>
2020-04-05 11:02:37 +0200

    client first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles["root-wrapper"]}>
+        Cheetah
+    </div>
+  );
+}
+
+export default App;
