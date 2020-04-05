41f25df345b88817a8e75c9202839a3ba18a844b
shimizacken <shimizacken@gmail.com>
2020-04-05 12:37:19 +0200

    randomAction added

--- client/src/features/chat/state/chatMessagesReducer.js
+++ client/src/features/chat/state/chatMessagesReducer.js
@@ -1,5 +1,14 @@
+import { PUBLISH_MESSAGE } from "./tests/chatMessagesConstants";
+
 const messagesInitialState = [];
 
-export const messages = (state = messagesInitialState) => {
+export const messages = (state = messagesInitialState, action) => {
+  if (action.type === PUBLISH_MESSAGE) {
+    return [
+      ...state,
+      action.message
+    ]
+  }
+
   return state;
 };
--- client/src/features/chat/state/tests/chatMessagesReducer.test.js
+++ client/src/features/chat/state/tests/chatMessagesReducer.test.js
@@ -1,7 +1,32 @@
+import { randomAction } from '../../../../state/randomAction';
 import { messages } from '../chatMessagesReducer';
+import { publishMessage } from './chatMessagesActions';
 
 describe('chatMessagesReducer test suite', () => {
+  describe('defaults', () => {
     it('should return default state', () => {
-        expect(messages()).toMatchSnapshot();
+      const action = randomAction();
+
+      expect(messages(undefined, action)).toMatchSnapshot();
     });
+  });
+
+  describe('publish message test suite', () => {
+    it('should add new message', () => {
+        const message = {
+          id: '1a2s3d4f5g',
+          text: 'hello all!'
+        };
+
+        expect(messages(undefined, publishMessage(message))).toMatchSnapshot();
+    });
+  });
+
+  describe('edit message test suite', () => {
+    it.todo('should add new message');
+  });
+
+  describe('delete message test suite', () => {
+    it.todo('should add new message');
+  });
 });
