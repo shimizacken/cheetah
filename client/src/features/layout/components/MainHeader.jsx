233ed1dd2df6f7ffac763b4b9e3b54ef393fb018
shimizacken <shimizacken@gmail.com>
2020-04-06 11:51:18 +0200

    placeholder colored

--- client/src/features/layout/components/MainHeader.jsx
+++ client/src/features/layout/components/MainHeader.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import img from '../../../assets/leopard_1f406.png';
+import defaultLogo from '../../../assets/default-logo-2.png';
+import smallLogo from '../../../assets/small-logo.png';
 import { HeaderDisplayMode } from '../bll/headerDisplayMode';
 import styles from './MainHeader.module.scss';
 
 export const MainHeader = ({ mode }) => {
   if (mode === HeaderDisplayMode.DEFAULT) {
-    return <img src={img} alt="Cheetah chat" />;
+    return <img src={defaultLogo} alt="Cheetah chat" />;
   }
 
   return (
     <div className={styles['main-header']}>
-      <img src={img} alt="Cheetah chat" width={50} className={styles.logo} />
+      <img src={smallLogo} alt="Cheetah chat" width={50} className={styles.logo} />
     </div>
   );
 };
