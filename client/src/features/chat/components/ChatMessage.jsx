8e5b3f6e0a2e4f01f1137359e9a837324b093450
shimizacken <shimizacken@gmail.com>
2020-04-06 07:27:14 +0200

    colors changed

--- client/src/features/chat/components/ChatMessage.jsx
+++ client/src/features/chat/components/ChatMessage.jsx
@@ -4,15 +4,15 @@ import { Dot } from '../../../components/common/dot/Dot';
 import styles from './ChatMessage.module.scss';
 
 export const ChatMessage = React.memo(({ userName, text, active, date, isCurrentUser }) => (
-  <div className={classNames(styles['message-root-wrapper'], isCurrentUser && styles['current-user-position'])}>
+  <div className={classNames(styles['message-root-wrapper'], isCurrentUser && styles['current-user-message'])}>
     <div className={styles['message-wrapper']}>
       <div className={styles.header}>
         <span className={styles['user-name']}>
-          {active && <Dot />} {userName}
+          {active && <Dot />} <span>{userName}</span>
         </span>
         <span className={styles['date']}>{date}</span>
       </div>
-      <div className={styles['content']}>{text}</div>
+      <div className={classNames(styles['content'], isCurrentUser && styles['current-user-message'])}>{text}</div>
     </div>
   </div>
 ));
