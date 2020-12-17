fad3217526997d065896330c7b5bde3587c100a3
shimizacken <shimizacken@gmail.com>
2020-12-17 21:45:30 +0100

    ts

--- client/src/features/theme/state/themeTypeMiddleware.ts
+++ client/src/features/theme/state/themeTypeMiddleware.ts
@@ -1,7 +1,8 @@
 import { TOGGLE_THEME } from './constants';
 import { themeStorageKey } from '../bll/themeStorageKey';
+import { Middleware } from 'redux';
 
-export const themeTypeMiddleware = () => (next) => (action) => {
+export const themeTypeMiddleware: Middleware = () => (next) => (action) => {
   if (action.type === TOGGLE_THEME) {
     window.localStorage.setItem(themeStorageKey, action.themeType);
   }
