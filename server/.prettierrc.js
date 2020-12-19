78a68b287617a5f814f492ed86613c860e7f46fc
shimizacken <shimizacken@gmail.com>
2020-12-19 13:30:13 +0100

    old server deleted

--- server/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'none',
-  printWidth: 80,
-  tabWidth: 2,
-  singleQuote: true,
-  useTabs: false,
-  bracketSpacing: false
-};
--- server/savannah/events.js
+++
@@ -1,7 +0,0 @@
-import {Server} from 'ws';
-
-const socket = new Server({port: 9555});
-
-socket.on('connection', (ws) => {
-  console.log('🚀 ~ file: events.ts', ws);
-});
